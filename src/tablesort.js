93b1daf1561be1056737a408dd63cf618caae997
RobertRish <robert.rish1225@gmail.com>
2022-09-18 15:40:33 -0400

    Added ternary function expression

--- src/tablesort.js
+++ src/tablesort.js
@@ -8,7 +8,7 @@
 
 function sortTableByColumn(table, column, asc = true) {
     // The word ternary means "composed of 3 parts".  Think of it as "ter" or "tre".
-    // The conditional (ternary) operator is the only JavaScript operator that takes three operands
+    // The conditional (ternary) operator is the only JavaScript operator that takes three operands.
     const dirModifier = asc ? 1 : -1; 
     const tBody = table.tBodies[0];
     const rows = Array.from(tBody.querySelectorAll("tr"));
