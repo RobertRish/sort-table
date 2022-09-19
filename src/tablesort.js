ded1e57141411eaf53d2805d3b0b0849371cae56
RobertRish <robert.rish1225@gmail.com>
2022-09-19 13:14:23 -0400

    testing revert changes feature in vs code

--- src/tablesort.js
+++ src/tablesort.js
@@ -40,5 +40,7 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
         const tableElement = headerCell.parentElement.parentElement.parentElement;
         const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
         const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending)
     })
 })
